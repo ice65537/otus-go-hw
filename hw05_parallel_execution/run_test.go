47464b8fd28bd53090f771a4ecb3084673ed2672
ice65537 <alexander.letuchev@gmail.com>
2023-01-30 00:15:38 +0300

    test2

--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -2,18 +2,16 @@ package hw05parallelexecution
 
 import (
 	"errors"
-	"fmt"
 	"math/rand"
 	"sync/atomic"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/require"
-	"go.uber.org/goleak"
 )
 
 func TestRun(t *testing.T) {
-	//defer goleak.VerifyNone(t)
+	// defer goleak.VerifyNone(t)
 
 	/*t.Run("if were errors in first M tasks, than finished not more N+M tasks", func(t *testing.T) {
 		tasksCount := 50

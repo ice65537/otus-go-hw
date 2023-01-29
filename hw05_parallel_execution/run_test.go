c3c2b2b4557c0db6180a3a78adbbfa549064bc42
ice65537 <alexander.letuchev@gmail.com>
2023-01-30 00:38:45 +0300

    223213

--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -15,32 +15,6 @@ import (
 func TestRun(t *testing.T) {
 	defer goleak.VerifyNone(t)
 
-	t.Run("if were errors in first M tasks, than finished not more N+M tasks", func(t *testing.T) {
-		tasksCount := 50
-		tasks := make([]Task, 0, tasksCount)
-
-		var runTasksCount int32
-
-		for i := 0; i < tasksCount; i++ {
-			err := fmt.Errorf("error from task %d", i)
-			tasks = append(tasks, func() error {
-				time.Sleep(time.Millisecond * time.Duration(rand.Intn(50)))
-				atomic.AddInt32(&runTasksCount, 1)
-				return err
-			})
-		}
-
-		workersCount := 3
-		maxErrorsCount := 13
-		err := Run(tasks, workersCount, maxErrorsCount)
-		fmt.Println("runTasksCount=", runTasksCount)
-		fmt.Println("int32(workersCount+maxErrorsCount)=", int32(workersCount+maxErrorsCount))
-		fmt.Println("Truef=", errors.Is(err, ErrErrorsLimitExceeded))
-
-		require.Truef(t, errors.Is(err, ErrErrorsLimitExceeded), "actual err - %v", err)
-		require.LessOrEqual(t, runTasksCount, int32(workersCount+maxErrorsCount), "extra tasks were started")
-	})
-
 	t.Run("tasks without errors", func(t *testing.T) {
 		tasksCount := 50
 		tasks := make([]Task, 0, tasksCount)
@@ -69,6 +43,32 @@ func TestRun(t *testing.T) {
 
 		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
 		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
+	})
+
+	t.Run("if were errors in first M tasks, than finished not more N+M tasks", func(t *testing.T) {
+		tasksCount := 50
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+
+		for i := 0; i < tasksCount; i++ {
+			err := fmt.Errorf("error from task %d", i)
+			tasks = append(tasks, func() error {
+				time.Sleep(time.Millisecond * time.Duration(rand.Intn(50)))
+				atomic.AddInt32(&runTasksCount, 1)
+				return err
+			})
+		}
+
+		workersCount := 3
+		maxErrorsCount := 13
+		err := Run(tasks, workersCount, maxErrorsCount)
+		fmt.Println("runTasksCount=", runTasksCount)
+		fmt.Println("int32(workersCount+maxErrorsCount)=", int32(workersCount+maxErrorsCount))
+		fmt.Println("Truef=", errors.Is(err, ErrErrorsLimitExceeded))
+
+		require.Truef(t, errors.Is(err, ErrErrorsLimitExceeded), "actual err - %v", err)
+		require.LessOrEqual(t, runTasksCount, int32(workersCount+maxErrorsCount), "extra tasks were started")
 	})
 
 	t.Run("Too much workers", func(t *testing.T) {

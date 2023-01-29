d070d6d616fe7cb7aadae300296dfdd35cf4ebe7
ice65537 <alexander.letuchev@gmail.com>
2023-01-29 23:27:35 +0300

    1st

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,14 +2,89 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"fmt"
+	"reflect"
+	"runtime"
 )
 
-var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
+var ErrErrorsLimitExceeded error
 
 type Task func() error
 
-// Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
-func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+type taskJob struct {
+	idx  int
+	task Task
+}
+
+type taskResult struct {
+	idx    int
+	err    error
+	worker string
+}
+
+// var taskQueue chan Task
+
+// Run starts tasks in numWorkers goroutines and stops its work when receiving maxErrors errors from tasks.
+func Run(tasks []Task, numWorkers, maxErrors int) error {
+	if numWorkers <= 1 {
+		numWorkers = 1 // Хотя бы один worker должен быть запущен
+	}
+	if maxErrors <= 0 {
+		maxErrors = len(tasks) + 100 // Обработка с игнорированием ошибок
+	}
+
+	jobQueue := make(chan taskJob, len(tasks))
+	for i, task := range tasks {
+		jobQueue <- taskJob{i, task}
+	}
+	close(jobQueue)
+
+	jobResult := make(chan taskResult, len(tasks))
+	massBreak := make(chan struct{})
+	breakFlag := false
+	defer close(jobResult)
+	defer close(massBreak)
+
+	for i := 1; i <= numWorkers; i++ {
+		go worker(fmt.Sprint("Worker", i), &jobQueue, &jobResult, &massBreak, &breakFlag)
+	}
+
+	successCount := 0
+	errorCount := 0
+	errorStream := ""
+	for successCount+errorCount < len(tasks) && errorCount < maxErrors {
+		jr := <-jobResult
+		jobDesc := fmt.Sprintf("\r\nTask[%d]{%s} by %s", jr.idx, runtime.FuncForPC(reflect.ValueOf(tasks[jr.idx]).Pointer()).Name(), jr.worker)
+		if jr.err != nil {
+			errorCount++
+			errorStream += jobDesc
+		} else {
+			successCount++
+		}
+	}
+
+	breakFlag = true
+	for i := 1; i <= numWorkers; i++ {
+		massBreak <- struct{}{}
+	}
+
+	if errorCount >= maxErrors {
+		ErrErrorsLimitExceeded = errors.New("Errors limit [" + fmt.Sprint(maxErrors) + "] exceeded:" + errorStream)
+		return ErrErrorsLimitExceeded
+	}
+
 	return nil
+}
+
+func worker(name string, jobQueue *chan taskJob, jobResult *chan taskResult, breaker *chan struct{}, breakFlag *bool) {
+	for {
+		select {
+		case <-(*breaker):
+			return
+		case jq, ok := <-(*jobQueue):
+			if ok && !*breakFlag {
+				(*jobResult) <- taskResult{jq.idx, jq.task(), name}
+			}
+		}
+	}
 }
--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -67,4 +67,128 @@ func TestRun(t *testing.T) {
 		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
 		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
 	})
+
+	t.Run("all tasks without errors break", func(t *testing.T) {
+		tasksCount := 30
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+
+		for i := 0; i < tasksCount; i++ {
+			tasks = append(tasks, func() error {
+				time.Sleep(50 * time.Millisecond)
+				atomic.AddInt32(&runTasksCount, 1)
+				return errors.New("Err")
+			})
+		}
+
+		workersCount := 5
+		maxErrorsCount := -7783
+
+		err := Run(tasks, workersCount, maxErrorsCount)
+		require.NoError(t, err)
+		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
+	})
+
+	t.Run("Too much workers", func(t *testing.T) {
+		tasksCount := 30
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+
+		for i := 0; i < tasksCount; i++ {
+			if i%2 == 0 {
+				tasks = append(tasks, func() error {
+					time.Sleep(50 * time.Millisecond)
+					atomic.AddInt32(&runTasksCount, 1)
+					return errors.New("Err")
+				})
+			} else {
+				tasks = append(tasks, func() error {
+					time.Sleep(50 * time.Millisecond)
+					atomic.AddInt32(&runTasksCount, 1)
+					return nil
+				})
+			}
+		}
+
+		workersCount := 300
+		maxErrorsCount := 20
+
+		err := Run(tasks, workersCount, maxErrorsCount)
+		require.NoError(t, err)
+		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
+	})
+
+	t.Run("Zero (one) worker", func(t *testing.T) {
+		tasksCount := 10
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+
+		for i := 0; i < tasksCount; i++ {
+			if i%2 == 0 {
+				tasks = append(tasks, func() error {
+					time.Sleep(50 * time.Millisecond)
+					atomic.AddInt32(&runTasksCount, 1)
+					return errors.New("Err")
+				})
+			} else {
+				tasks = append(tasks, func() error {
+					time.Sleep(50 * time.Millisecond)
+					atomic.AddInt32(&runTasksCount, 1)
+					return nil
+				})
+			}
+		}
+
+		workersCount := 1
+		maxErrorsCount := -1
+
+		err := Run(tasks, workersCount, maxErrorsCount)
+		require.NoError(t, err)
+		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
+	})
+
+	t.Run("Zero (one) worker and Nonsequental test", func(t *testing.T) {
+		tasksCount := 100
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+
+		for i := 0; i < tasksCount; i++ {
+			if i%2 == 0 {
+				tasks = append(tasks, func() error {
+					time.Sleep(50 * time.Millisecond)
+					atomic.AddInt32(&runTasksCount, 1)
+					return errors.New("Err")
+				})
+			} else {
+				tasks = append(tasks, func() error {
+					time.Sleep(50 * time.Millisecond)
+					atomic.AddInt32(&runTasksCount, 1)
+					return nil
+				})
+			}
+		}
+
+		runTasksCount = 0
+		workersCount := -45  // equal to 1
+		maxErrorsCount := -1 // ignore errors
+		start := time.Now()
+		err := Run(tasks, workersCount, maxErrorsCount)
+		elapsedTime := time.Since(start)
+		require.NoError(t, err)
+		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed at 1st run")
+
+		runTasksCount = 0
+		workersCount = 50
+		maxErrorsCount = -1 // ignore errors
+		start = time.Now()
+		err = Run(tasks, workersCount, maxErrorsCount)
+		elapsedTime2 := time.Since(start)
+		require.NoError(t, err)
+		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed at 2nd run")
+		require.LessOrEqual(t, int64(elapsedTime2), int64(elapsedTime), "tasks were run sequentially?")
+	})
 }

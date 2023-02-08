7022bd6d0f23824580f6953d597d131a73ffe29c
ice65537 <alexander.letuchev@gmail.com>
2023-02-08 22:24:02 +0300

    1st

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,44 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	out := make([]Out, len(stages)+1)
+	out[0] = in
+	for i, stage := range stages {
+		select {
+		case <-done:
+			return nil
+		default:
+		}
+		out[i+1] = stage(out[i])
+	}
+	return out[len(stages)]
 }
+
+/*func worker(name string, jobQueue In, jobDo Stage, brk In, bufferSize int) Out {
+	var jq, jr interface{}
+	jobResult := make(Bi, bufferSize)
+	go func() {
+		defer close(jobResult)
+		jget := false
+		for {
+			select {
+			case <-brk:
+				return
+			case jq, jget = <-jobQueue:
+			}
+			if jget {
+				jr = jobDo(jq)
+				select {
+				case <-brk:
+					return
+				case jobResult <- jr:
+				}
+			}
+		}
+	}()
+	return jobResult
+}
+
+func getFuncName(i interface{}) string {
+	return runtime.FuncForPC(reflect.ValueOf(i).Pointer()).Name()
+}*/

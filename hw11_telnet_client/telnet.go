61436c46b95c108d247286c68029cf034ee5b1cd
ice65537 <alexander.letuchev@gmail.com>
2023-05-08 13:52:43 +0300

    1st

--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"io"
+	"net"
 	"time"
 )
 
@@ -12,10 +14,57 @@ type TelnetClient interface {
 	Receive() error
 }
 
+type Cli struct {
+	address   string
+	timeout   time.Duration
+	in        io.ReadCloser
+	out       io.Writer
+	session   net.Conn
+	inBuffer  []byte
+	outBuffer []byte
+}
+
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+	instance := Cli{address: address, timeout: timeout, in: in, out: out, session: nil}
+	if instance.timeout == 0 {
+		instance.timeout = 10 * time.Second
+	}
+	instance.inBuffer = make([]byte, 1024, 2048)
+	instance.outBuffer = make([]byte, 1024, 2048)
+	return instance
+}
+
+func (self Cli) Connect() error {
+	var err error
+	if self.session, err = net.DialTimeout("TCP", self.address, self.timeout); err != nil {
+		return err
+	}
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (self Cli) Send() error {
+	n, err := self.in.Read(self.outBuffer)
+	if err != nil {
+		return err
+	}
+	if _, err = self.session.Write(self.outBuffer[:n]); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (self Cli) Receive() error {
+	n, err := self.session.Read(self.inBuffer)
+	if err != nil {
+		return err
+	}
+	fmt.Print(self.inBuffer[:n])
+	return nil
+}
+
+func (self Cli) Close() error {
+	if err := self.session.Close(); err != nil {
+		return err
+	}
+	return nil
+}

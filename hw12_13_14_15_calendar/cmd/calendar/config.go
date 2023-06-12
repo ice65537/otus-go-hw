8db813799a2bdbb7f90fb7eb6c0646f877fa2505
ice65537 <alexander.letuchev@gmail.com>
2023-06-12 18:09:30 +0300

    1809

--- hw12_13_14_15_calendar/cmd/calendar/config.go
+++ hw12_13_14_15_calendar/cmd/calendar/config.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
 // При желании конфигурацию можно вынести в internal/config.
 // Организация конфига в main принуждает нас сужать API компонентов, использовать
 // при их конструировании только необходимые параметры, а также уменьшает вероятность циклической зависимости.
@@ -14,7 +20,16 @@ type LoggerConf struct {
 }
 
 func NewConfig() Config {
-	return Config{}
+	viper.SetConfigName(configFile)
+	viper.SetConfigType("toml")
+	viper.AddConfigPath("../configs/config.toml")
+	if err := viper.ReadInConfig(); err != nil {
+		panic(fmt.Errorf("config reading fatal error: %w", err))
+	}
+	cfgParsed := viper.AllSettings()
+	fmt.Println(cfgParsed)
+	cfg := Config{}
+	return cfg
 }
 
 // TODO
--- hw12_13_14_15_calendar/cmd/calendar/version.go
+++ hw12_13_14_15_calendar/cmd/calendar/version.go
@@ -7,9 +7,9 @@ import (
 )
 
 var (
-	release   = "UNKNOWN"
-	buildDate = "UNKNOWN"
-	gitHash   = "UNKNOWN"
+	release   = "12.1"
+	buildDate = "2023-06-12"
+	gitHash   = "6a8c53d1692a79da3bb2f23ecc6bbb91e695996f"
 )
 
 func printVersion() {
